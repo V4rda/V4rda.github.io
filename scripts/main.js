cf364b8a531377b9e280bb224fd6361f98a3e8d1
V4rda <59003469+V4rda@users.noreply.github.com>
2019-12-18 00:51:20 -0300

    WTF should i draw

---
+++ scripts/main.js
@@ -0,0 +1,221 @@
+
+$(function() {
+
+WTF.init({
+
+     heading: [
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+         "Sei lá, desenha essa merda aqui",
+         "Se liga na idéia",
+	"Mano eu tava chapado ai pensei em desenhar",
+	"Mano, eu tava muito chapado ai pensei em desenhar",
+	"Mano, eu tava muito, tipo, MUITO chapado, ai pensei em desenhar",
+	"Que tal se você desenhasse",
+	"Eu sei que você desenha bem mas você consegue desenhar",
+	"Se eu queimar 4 pedras de crack talvez eu consiga desenhar",
+	"Caralho mano, eu acho que seria incrível se eu desenhasse",
+	"Depois de desenhar sua mãe pelada eu acho que vou desenhar",
+	"Eu não quero ver o resultado, desenha",
+	"POR FAVOR O CARA QUE PROGRAMOU ISSO NÃO TEM AMIGOS ELE PRECISA DE AJUDA MANDA UM ZAP PRA +55 32 998210476 COM FOTO DA TETA, ou vc pode simplesmente ignorar tudo e desenhar"
+     ],
+     response: [
+         "Infelizmente eu ja vi essa porra",
+         "Me dá outro porra",
+         "Que merda é essa??",
+	"Sério, porra?",
+	"CARALHO????",
+	"Quem foi o autista que fez esse site?",
+	"Minha religião não permite",
+	"MINHA MÃE TÁ DO MEU LADO.",
+	"Minha moral não é duvidosa o suficiente pra isso",
+	"meu pinto ficou muito duro com essa"
+     ],
+     template: [
+         "@guys com um puta @animal" como companheiro",
+	"@guys fazendo cosplay de @guys",
+	"@guys vs. @guys"
+	"@animal @color com @ill",
+	"Um furry de @animal",
+	"@guys com fetiche em @object",
+	"@guys mas em uma paleta de @color",
+	"Um transformer que vira @animal",
+	"Um transformer que vira @object",
+	"@guys 3: o retorno de @guys",
+	"@guys desenhado pelo Tim Burton",
+	"@guys dançando @music",
+	"@guys e @guys dançando @music",
+	"@guys só que @adj",
+	"@guys @adj",
+	"@guys @act",
+	"@guys @act com @animal",
+	"@guys @adj @act",
+	"@animal @adj",
+	"@guys tem uma coleção de @object",
+	"@guys @adj porquê viu um @animal @act com @guys",
+	"@guys @adj porquê não conseguiu cancelar ninguém no twitter hoje",
+	"a chance de sair essa frase é muito baixa, mas eu tenho certeza que você não teria chegado aqui se você não gostasse muito de @guys",
+	"@guys @act quatro clones de @guys",
+	"@guys @guys @guys @guys @guys, essa não é uma opção válida, é só um teste, será se 2 desses 5 nomes são iguais? se forem desenha essa pessoa pelada",
+	"@guys pelado só de meias",
+	"A visão mais broxante possível de @guys",	
+	"@guys depois de 40 dias no deserto",
+	"E se Hitler fosse @guys ?"
+	"E se Hitler fosse um @animal ?",
+	"E se jesus fosse um @animal ?",
+	"@guys depois de um horrível acidente de moto sem capacete"
+ 
+	
+     ],
+     
+guys: [
+         "Hitler","Stalin","Che Guevara","alguma puta", "a ex do seu amigo", 
+	"sua ex", "loli nazista", "uma egirl", " Machado de Assis", "Elon Musk", 
+	"Nando Moura", "Olavo de Carvalho", "Ratinho", "Faustão",  "Naruto", 
+	"Goku supersayajin 3", "Jesus", "Britney Spears", "Ariana Grande", 
+	"Taylor Swift","Kim Kardashian", "Lúcifer", "Batman", "Coringa", 
+	"Arlequina", "Buddha", "alguem da akatsuki", "João Frango",
+	"aquele goleiro do flamengo que foi preso", "um emo de cemitério",
+	"Pabblo Vitar", "Ana Maria Braga", "Luciano Huck", "Glória Maria",
+	"Tadeu smith... shumit... shemit... mishit... enfim, aquele cara do fantástico",
+	"Marília Gabriela", "Algum vilão secundário que ninguém liga", "Billie Joe Armstrong",
+	"Slash", "o mascote da sadia", "Xuxa Meneghel", "Lula", "Bolsonaro", "Ednaldo Pereira", 
+	"Michael Jackson", "Gretchen", "um pokemon", "uma radfem", "billie eilish", "aquelas minas de cabelo curto que fazem cover no youtube", "Erick Jacquin", "Heath Ledger", "Chorão", "Tio Orochi", "algum youtuber falido que fazia vídeos de minecraft em 2013", "Venom_Extreme", "Monark", "Richard Rasmussem", "algum japones do BTS", "Jesus Negro", "Hulk Agiota", "Marquito do Ratinho", "Gugu Liberato depois de entrar em coma", "macaco robô", "um índio genérico", "um judeu alemão, só que depois do holocausto", "Supla", "iCarly", "Cardi B", "Amy Winehouse", "Fábio Porchat", "um negro da Ku Klux Klan", "Sakura", "Tsunade", "Jiraya", "Kuririn", "um ganso"
+    ],
+     
+animal: [ "mamute", "dinossauro", "tigre dente de sabres", "rato gigante com 3 cabeças e espinhos", "veado emo", "caralho voador", "espermatozóide atômico", "... sei lá, usa sua imaginação", "gatinho cacto", "nyan cat psicopata", "galinha de kalsa", "pangolim", "o pato do lanso a braba fdp", "ganso", "ornitorrinco", "dragão", "um pokemon", "unicórnio", "lêmure", "Harambe", "arara depenada", "Mr. Toucan", "gato de botas", "corvo"
+	],
+color: [
+         "vermelho","azul", "verde", "amarelo", "cinza, "preto", "rosa", "branco", "roxo", "turquesa", "púrpura", "magenta", "verde cabelo-de-egirl"
+	]
+object: [ 
+	 "vaselina", "dentadura", "cacto", "bronzeador em spray", "botijão de gás", "mouse com luzinha", "torradeira", "um vinil do My Chemical Romance", "máquina de waffle", "mixer", "batedor", "batedor de ovos", "espátula", "cutelo", "faca cega", "peixeira", "vara de pescar", "foto do pau indesejada", "nude da gretchen", "indie de corno", "câmara de gás", "keytarra da lady gaga", "arpão", "espingarda", "AR-15", "prego da mão direita de Jesus", "Judeu"
+	]
+adj: [
+	"fofo", "bonito", "feio", "corno", "triste", "decepcionado", "banguela", "feliz", "com as séries em dia", "com cabelo bagunçado", "com camiseta do PT", "judeu", "muçulmano", "negro", "asiático", "suicida", "que gosta de filmes da marvel" 
+"careca", "plantando bananeira"
+	]
+music: [
+	"sertanejo", "funk", "rock", "musica emo", "eletrônica", "country", "reggae", "forró", "samba", "pagode", "kpop", "ela_partiu_tim_maia.mp3", "darude - sandstorm", "O Sol - Victor Kley", "BTS" "indie de corno", "Nickelback", "a trilha sonora de Pulp Fiction", "dance anos 70", "ABBA"
+	]
+act: [
+	"dançando", "transando", "fudendo", "cozinhando", "sendo pregado em uma cruz", "tocando cavaco", "sabotando sistemas de segurança de uma multinacional", "comendo cu de quem tá lendo", "te chupando bem gostoso", "resetando o modem porquê a internet tá uma merda", "jogando CS:GO", "jogando Minecraft", "comendo cream cracker", "assistindo Lost", "assistindo Naruto", "assistindo Bakugan", "assistindo Beyblade: Metal Masters", "lendo Caras", "lendo O Antagonista", "fazendo a revolução", "maratonando Todo Mundo Odeia o Chris", "dormindo", "dando um mortal triplo carpado pra trás enquanto faz uma releitura do Colosso de Rhodes no tricot", "virando uma 51", "xingando egirl no twitter", "matching buts e com a calça do conjunto do jaco do seu NAMO kkkkkk", "ostentando gucci falsificada", "compartilhando seringa", "usando heroína", "usando crack", "cheirando pó", "queimando um finim", "ouvindo restart", "ouvindo now united", "ouvindo indie de corno"
+]  
+});
+
+    // Populate using a JSON file
+WTF.init( 'sample.json' );
+
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // @see https://support.google.com/drive/answer/37579?hl=en
+    //WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+
+});
